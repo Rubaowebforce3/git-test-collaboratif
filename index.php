de8b536ed463b00886dc1227c321a85030b19f38
TrevorColas <trevor.colas@yahoo.fr>
2022-05-13 16:18:23 +0200

    modification index

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+require_once ('./manager.php');
+require_once ('./secretaire.php');
+
+$al = new Manager('Capone', 'Al', 32);
+$sylvie = new Secretaire('Durand', 'Sylvie', 25);
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -6,21 +15,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Test colaboratif</title>
+    <div>
+    <?php 
+        $al->direBonjour();
+        echo "<br><br>";
+        $sylvie->direBonjour();
+        echo "<br><br>";
+        $sylvie->prendRDV($al, "Eliott Ness à 3 heures du mat. sur les docks...");
+        $al->afficherRDV();
+    ?>
+    <div>
 </head>
 <body>
-    
-
-
-
-
-
-
-
-
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-</body>
-</html>
